a8879c0ccea8b011d7292253f8d837f88870a842
ManuelMuncaster <russmunc061@ed.amdsb.ca>
2016-11-01 13:32:49 -0400

    Version #2

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -12,28 +12,66 @@ namespace OneArmedBandit
     public partial class Form1 : Form
     {
         // random number generator
-
+        Random randGen = new Random();
         // int value for score initialized to 10
-
+        int score = 10;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void spinButton_Click(object sender, EventArgs e) 
+        private void spinButton_Click(object sender, EventArgs e)
         {
             // get random values for each reel (store each in separate int variable)
-
-
+            int reel1Value = randGen.Next(1, 4);
+            int reel2Value = randGen.Next(1, 4);
+            int reel3Value = randGen.Next(1, 4);
+           
             // check value of reel 1 with a switch statement and set appropriate image
 
+            switch (reel1Value)
+            {
+                case 1:
+                    reel1.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    reel1.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    reel1.Image = Properties.Resources._7_100x125;
+                    break;
+            }
 
             // check value of reel 2 with a switch statement and set appropriate image
 
-
+            switch (reel2Value)
+            {
+                case 1:
+                    reel2.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    reel2.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    reel2.Image = Properties.Resources._7_100x125;
+                    break; 
+            }
+           
             // check value of reel 3 with a switch statement and set appropriate image
 
+            switch (reel3Value)
+            {
+                case 1:
+                    reel3.Image = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    reel3.Image = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    reel3.Image = Properties.Resources._7_100x125;
+                    break;
+            }
 
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
@@ -45,12 +83,28 @@ namespace OneArmedBandit
             // If yes show "winner" statement and add 3 to score.
             // If no show "play again" statement and subtract 1 from score.         
 
+            if (reel1Value == reel2Value && reel3Value ==reel2Value && reel1Value == reel3Value)
+            {
+                outputLabel.Text = "Winner!!";
+                score = score + 3;
+            }
+            else if (reel1Value != reel2Value && reel3Value != reel1Value && reel3Value != reel2Value)
+            {
+                outputLabel.Text = "Play Again!";
+                score = score -1;
+            }
+                    
 
             // if score has reached 0 display "lose" message and set button enabled property to false
-
+            if (score == 0)
+            {
+                outputLabel.Text = "You lose!";
+                spinButton.Visible = false;
+            }
 
             // display updated score
 
+            scoreDisplay.Text = "" + score;
         }
     }
 }
